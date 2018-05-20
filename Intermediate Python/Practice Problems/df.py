7007d38abc94728f1beb9b105bd42c59be93b5fa
Anthony Perez <stockcart21@yahoo.com>
2018-05-20 14:17:32 -0700

    Create df.py

---
+++ Intermediate Python/Practice Problems/df.py
@@ -0,0 +1,14 @@
+for j, q in df.iterrows():
+   print(j)
+   print(q)
+   
+output: 
+      a
+      X 2
+      Y 15
+      Name: a, dtype: int64
+      b
+      X 8
+      Y 10
+      Name: b, dtype: int64
+      
