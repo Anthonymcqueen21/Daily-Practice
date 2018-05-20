f5e9dc08549a4dba81f8ae40fbf34c575e6a0f09
Anthony Perez <stockcart21@yahoo.com>
2018-05-20 14:02:32 -0700

    Create Mobile.py

---
+++ Intermediate Python/Practice Problems/Mobile.py
@@ -0,0 +1,6 @@
+sessions['Mobile'] = sessions['Device'].apply(str.capitalize)
+print(sessions)
+
+   DEVICE active login_id Mobile
+a  MOBILE yes    12347    Mobile
+b  DESKTOP no    11021    Desktop
