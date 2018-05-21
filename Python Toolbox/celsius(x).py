6ff117b5ea25ca0b22bdf3df2765d59ff191a028
Anthony Perez <stockcart21@yahoo.com>
2018-05-21 09:23:42 -0700

    Create celsius(x).py

---
+++ Python Toolbox/celsius(x).py
@@ -0,0 +1,8 @@
+def celsius(x):
+   """Converts the temparature from Fahrenheit to Celsius"""
+   y = (x - 32) * 5 / 9
+   return y
+# Convert 100 degrees Farenheit to Celcius
+print(celsius(100))
+
+Incorret answer
