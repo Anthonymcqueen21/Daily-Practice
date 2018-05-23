afa16637f8abb1450677e91fcb3bcd556d15b899
Anthony Perez <stockcart21@yahoo.com>
2018-05-22 20:55:20 -0700

    Create Names.py

---
+++ Python Toolbox 2/Names.py
@@ -0,0 +1,10 @@
+arrow_vil = ['Slade Wilson','Isabel Rochev','Sebastian Blood']
+for x in enumerate(arrow_vil, start = 8):
+    print(str(x) + ': ' + j)
+    
+    
+output: 
+
+8. Slade Wilson
+9. Isabel Rochev
+10. Sebastian Blood
