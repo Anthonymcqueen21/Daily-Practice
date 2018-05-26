bc9a00137cde25229ed3f1a8ff8b60506abec4d6
Anthony Perez <stockcart21@yahoo.com>
2018-05-25 22:00:33 -0700

    Create wc_results.py

---
+++ Intermediate Python/wc_results.py
@@ -0,0 +1,19 @@
+team  titles  runners_up  third_place  fourth_place
+0     Brazil       5           2            2             2
+1    Germany       4           4            4             1
+2      Italy       4           2            1             1
+3  Argentina       2           3            0             0
+4    Uruguay       2           0            0             3
+5     France       1           1            2             1
+
+print(fifa_wc_results.iloc[:,1]
+
+output:
+
+0    5
+1    4
+2    4
+3    2
+4    2
+5    1
+Name: titles, dtype: int64
