8137c62a12fe4cdee324c377d814850c687ef1fd
Anthony Perez <stockcart21@yahoo.com>
2018-05-27 11:08:40 -0700

    Create A, B.py

---
+++ Cleaning Data/A, B.py
@@ -0,0 +1,21 @@
+This is the DataFrame df:
+
+      A     B
+0  12.0  32.0
+1  63.0   NaN
+2   NaN  23.0
+3  24.0  41.0
+4  12.0  31.0
+
+print(df.drop_duplicates())
+
+output: 
+
+This is the DataFrame df:
+
+      A     B
+0  12.0  32.0
+1  63.0   NaN
+2   NaN  23.0
+3  24.0  41.0
+4  12.0  31.0
