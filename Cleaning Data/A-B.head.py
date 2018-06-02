0a93bf17cd1aa4639557e10146946fa957f628a2
Anthony Perez <stockcart21@yahoo.com>
2018-06-02 16:55:34 -0700

    Create A-B.head.py

---
+++ Cleaning Data/A-B.head.py
@@ -0,0 +1,22 @@
+Here is a preview of the DataFrame df:
+
+    A   B
+0  11  32
+1  12  45
+2  92  62
+3  56  34
+4  32  63
+5  41  45
+6  56  72
+
+Code:
+print(df.head())
+
+output: 
+  A  B
+0 11 32
+1 12 45
+2 92 62
+3 56 34
+4 32 63
+
