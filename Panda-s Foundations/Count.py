4a51ea811a5e645a3b88fd127f74f1bb64e2c3db
Anthony Perez <stockcart21@yahoo.com>
2018-06-11 21:27:12 -0700

    Create Count.py
    You have the following DataFrame df:

---
+++ Panda-s Foundations/Count.py
@@ -0,0 +1,16 @@
+Month  Count
+0   Jan     52
+1   Apr     29
+2   Mar     46
+3   Feb      3
+
+Script:
+# Get the standard devation of 'Count'
+print(df['Count'].std())
+
+Output:
+0 3
+1 29
+2 46
+3 52
+dtype: int64
