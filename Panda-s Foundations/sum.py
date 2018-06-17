69edd8d3d8a732793e764c8b1e2bf110e0c811d6
Anthony Perez <stockcart21@yahoo.com>
2018-06-17 15:58:30 -0700

    Create sum.py

---
+++ Panda-s Foundations/sum.py
@@ -0,0 +1,14 @@
+You have the following DataFrame df:
+
+  Month  Count
+0   Jan     52
+1   Apr     29
+2   Mar     46
+3   Feb      3
+
+Script:
+# get the sum of 'Count'
+print(df['Count'].sum())
+
+Output:
+130
