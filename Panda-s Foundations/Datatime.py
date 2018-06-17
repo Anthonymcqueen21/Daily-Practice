70650bcb20595975340094995bbada64bd262709
Anthony Perez <stockcart21@yahoo.com>
2018-06-16 22:22:08 -0700

    Create Datatime.py

---
+++ Panda-s Foundations/Datatime.py
@@ -0,0 +1,8 @@
+OUTPUT
+
+DatetimeIndex(['2016-01-01 09:12:34', '2016-01-01 09:12:34'], dtype='datetime64[ns]', freq=None)
+
+Script:
+l = ['2016-01-01 091234','2016-01-01 091234']
+print((pd.to_datatime(l)))
+
