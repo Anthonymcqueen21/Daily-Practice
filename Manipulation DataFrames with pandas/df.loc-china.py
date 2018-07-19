202c20bb337befa3f1a224c3a87532f183c33411
Anthony Perez <stockcart21@yahoo.com>
2018-07-19 08:43:48 -0700

    Create df.loc-china.py

---
+++ Manipulation DataFrames with pandas/df.loc-china.py
@@ -0,0 +1,21 @@
+You have the following DataFrame df:
+
+              eggs  salt  spam
+apr portugal    44    18     5
+    usa         36    95    63
+feb france      93    56    10
+    spain       67    20    88
+jan england     90    33    93
+    ireland     62    21    94
+jun germany     96    75    79
+    italy       52    99     7
+mar china       83    43    19
+    india        5     6    98
+    
+
+Code:
+print(df.loc[[('mar', 'china')]])
+
+Output:
+    eggs salt spam
+mar china 83 43 19
