f33e097eb2447d53e64a40a9bad80e29c46e5837
Anthony Perez <stockcart21@yahoo.com>
2018-07-19 08:54:13 -0700

    Create df.mar.py

---
+++ Manipulation DataFrames with pandas/df.mar.py
@@ -0,0 +1,16 @@
+You have the following DataFrame df:
+
+       eggs  salt  spam
+month                  
+jan      47  12.0    17
+feb     110  50.0    31
+mar     221  89.0    72
+apr      77  87.0    20
+may     132   0.0    52
+jun     205  60.0    55
+
+Code:
+print(df.loc['mar','eggs'])
+
+Output:
+221
