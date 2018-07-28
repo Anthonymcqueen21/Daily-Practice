3aa592a4876069effe009b2455d26db1833f53c6
Anthony Perez <stockcart21@yahoo.com>
2018-07-27 21:31:28 -0700

    Create Room.py

---
+++ Intermediate Python/Room.py
@@ -0,0 +1,12 @@
+Script:
+width = [15.07, 15.56, 0.9, 15.33]
+for index, z in enumerate(width):
+    print('Room ' + str(index + 1) + '-', str(z))
+    
+
+Output:
+Room 1 - 15.07
+Room 2 - 15.56
+Room 3 - 0.9
+Room 4 - 15.33
+
