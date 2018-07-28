490d5513f22f3b99abd58953fb8273800046e002
Anthony Perez <stockcart21@yahoo.com>
2018-07-27 22:03:44 -0700

    Create Saturn.py

---
+++ Intermediate Python/Saturn.py
@@ -0,0 +1,7 @@
+Script:
+rotation_period = {'earth':1, 'jupiter':0.415,'saturn':44}
+rotation_period['saturn'] = 0.445
+print(rotation_period)
+
+Output:
+{'saturn':0.445, 'jupiter':0.415, 'earth':1}
