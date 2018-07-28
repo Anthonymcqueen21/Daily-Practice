a6bf31ef7b060155bea9cd51db9c529d8f412341
Anthony Perez <stockcart21@yahoo.com>
2018-07-27 22:08:37 -0700

    Create Element.py

---
+++ Intermediate Python/Element.py
@@ -0,0 +1,12 @@
+Script:
+q = [19.98, 13.29, 18.61, 17.84]
+for k, y in enumerate(q):
+   print('Element' + str(k) + ' -', str(y))
+   
+Output:
+Element 0 - 19.98
+Element 1 - 13.29
+Element 2 - 18.61
+Element 3 - 17.84
+
+
