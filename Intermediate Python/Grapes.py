f8170859aff1267225566d75da1202cf0b536f7f
Anthony Perez <stockcart21@yahoo.com>
2018-07-27 21:27:22 -0700

    Create Grapes.py

---
+++ Intermediate Python/Grapes.py
@@ -0,0 +1,13 @@
+Script:
+
+fruits = {
+    'apples': {'cost':3, 'units':100},
+    'bananas': {'cost':1, 'units':80},
+    'grapes': {'cost':5, 'units':500}
+}
+
+print(fruits['grapes']['units'])
+
+
+Output:
+500
